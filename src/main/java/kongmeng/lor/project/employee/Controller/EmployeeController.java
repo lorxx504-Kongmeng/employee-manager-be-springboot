02e528ee408e428e49e8a36fed21c3b3f0db8f39
kongmenglor <lorxx504@umn.edu>
2023-02-27 18:25:40 -0600

    updating

--- src/main/java/kongmeng/lor/project/employee/Controller/EmployeeController.java
+++ src/main/java/kongmeng/lor/project/employee/Controller/EmployeeController.java
@@ -21,8 +21,6 @@ public class EmployeeController {
         List<Employee> employees = employeeService.findAllEmployees();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
-
-
     @GetMapping("/find/{id}")
     public ResponseEntity<Employee> getEmployeeId(@PathVariable("id") Long id) {
         Employee employee = employeeService.findEmployeeById(id);
